b3e7d3a2e7c17f6ff71e5f13096daeda68b23a30
rustjesus <157120247+rustjesus@users.noreply.github.com>
2024-02-06 23:54:08 -0600

    Part 2
    Part 2

---
+++ Assets_GunTutPt2/PlayerManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [HideInInspector] public static int currentAmmo;
+    [HideInInspector] public static int currentMaxAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
