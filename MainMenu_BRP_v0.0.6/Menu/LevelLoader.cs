22f9619a0046424cc7c25d02e4c5a28bbe4b2e24
rustjesus <157120247+rustjesus@users.noreply.github.com>
2024-02-16 14:53:14 -0600

    Part 7
    Part 7

---
+++ MainMenu_BRP_v0.0.6/Menu/LevelLoader.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Image loadingFill;
+    [SerializeField] private TextMeshProUGUI percentText;
+    [SerializeField] private TextMeshProUGUI levelToLoadTxt;
+    public void LoadLevel(string sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously(string sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        levelToLoadTxt.text = sceneIndex;
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            loadingFill.fillAmount = progress;
+            float percent = progress * 100;
+            percentText.text = percent.ToString("n0") + "%";
+
+            yield return null;
+
+        }
+    }
+
+    public void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+        Time.timeScale = 1.0f;  
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void DeletePrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
